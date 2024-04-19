1d929250318b2ef3ebdd0296d04c128b158d1d67
elvi93 <elvintopalovic@gmail.com>
2024-04-19 15:06:45 +0200

    done

---
+++ weeks/week-1/project.01/README.md
@@ -0,0 +1,20 @@
+# Elvin`s Ironskydive Lab
+
+This is a cool project that im doing about HTML to learn a bit about the language
+
+Hello Class
+
+
+
+### Commands to push to github
+
+git add .
+git commit -m "descriptive message about recent changes"
+git push original main
+
+
+### Commands to push to github ´chaind´
+
+git add .
+git commit -m "descriptive message about recent changes"
+git push original main
---
+++ weeks/week-3/tuesday/index.js
@@ -0,0 +1,39 @@
+const cafeName = "central perk" ;
+console.log(`Welcome to ${cafeName}! What would you like to order today?`);
+
+let priceOfCoffe = 2 ;
+let howManyCoffe = 2 ; 
+let total = priceOfCoffe * howManyCoffe
+console.log(`There you go, that'll be ${total} euros`);
+
+let isBestCoffee = true;
+
+if (isBestCoffee) {
+    console.log("You said this coffee is the best, and that was actually true.");
+} else {
+    console.log("You said this coffee is the best, but that was not true.");
+}
+
+
+
+var cafeGuests = 20;
+
+console.log("Initial number of cafe guests:", cafeGuests);
+
+
+cafeGuests = 15;
+
+
+console.log("New number of cafe guests:", cafeGuests);
+
+
+const maxGuests = 25;
+console.log("our place of guests:", maxGuests);
+
+// const maxGuests = 20;
+//console.log("max nummber of guests:", maxGuests);
+
+let myString = "coding is fun"
+console.log(myString.toLocaleUpperCase);
+console.log(myString.toLowerCase);
+
