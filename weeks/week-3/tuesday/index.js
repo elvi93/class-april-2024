cb222577e98f94ee6fc1c917f3f485097452fcf9
elvi93 <elvintopalovic@gmail.com>
2024-04-20 14:23:51 +0200

    main

--- weeks/week-3/tuesday/index.js
+++ weeks/week-3/tuesday/index.js
@@ -37,3 +37,123 @@ let myString = "coding is fun"
 console.log(myString.toLocaleUpperCase);
 console.log(myString.toLowerCase);
 
+
+
+
+
+
+
+
+
+let myNumber;
+
+myNumber = 20;
+
+myNumber = myNumber + 1;
+
+console.log(myNumber);
+
+myNumber +=1;
+
+console.log(myNumber);
+
+myNumber++;
+
+console.log(myNumber);
+console.log(myNumber +1);
+
+
+
+let varOne = 3;
+let varTwo = "3";
+
+console.log(varOne == varTwo)
+
+console.log(varOne === varTwo);
+
+console.log(varOne>varTwo);
+
+console.log(varOne>=7);
+
+console.log(varOne===varTwo || varOne <= 3 );
+
+
+let totalResult = 0;
+
+for (let i = 0; i <= 100; i++) {
+    totalResult += i;
+}
+
+console.log("The total result is:", totalResult);
+
+
+
+
+
+let number = 2;
+
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+
+}
+
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+
+
+
+function person(name, age, livesIn) {
+    if (livesIn.livesInCity) {
+        return true;
+    } else if (livesIn.livesInTown) {
+        return false;
+    }
+}
+
+
+function describePerson(name, age, livesIn) {
+    const place = person(name, age, livesIn) ? "city" : "town";
+    return `${name} is ${age} years old and lives in the ${place}.`;
+}
+
+function person(name, age, livesIn) {
+    if (livesIn.livesInCity) {
+        return true;
+    } else if (livesIn.livesInTown) {
+        return false;
+    }
+}
+
+const personDetails = {
+    name: "Elvin",
+    age: 31,
+    livesIn: {
+        livesInCity: true,
+        livesInTown: false
+    }
+};
+
+console.log(describePerson(personDetails.name, personDetails.age, personDetails.livesIn));
+
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+
+function calculateCircleArea(radius) {
+    return Math.PI * radius ** 2;
+}
+
+// Example usage:
+const radius = 5;
+const area = calculateCircleArea(radius);
+console.log("The area of the circle with radius", radius, "is", area.toFixed(2));
+
