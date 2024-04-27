d61839fb72ad66177534886873d3f5da63ad1b72
elvi93 <elvintopalovic@gmail.com>
2024-04-27 09:40:09 +0200

    done

--- weeks/week-3/tuesday/index.js
+++ weeks/week-3/tuesday/index.js
@@ -1,159 +1,44 @@
-const cafeName = "central perk" ;
-console.log(`Welcome to ${cafeName}! What would you like to order today?`);
-
-let priceOfCoffe = 2 ;
-let howManyCoffe = 2 ; 
-let total = priceOfCoffe * howManyCoffe
-console.log(`There you go, that'll be ${total} euros`);
-
-let isBestCoffee = true;
-
-if (isBestCoffee) {
-    console.log("You said this coffee is the best, and that was actually true.");
-} else {
-    console.log("You said this coffee is the best, but that was not true.");
+function multiply(num1, num2, callback) {
+    const result = num1 * num2;
+    callback(result);
 }
 
 
-
-var cafeGuests = 20;
-
-console.log("Initial number of cafe guests:", cafeGuests);
-
-
-cafeGuests = 15;
-
-
-console.log("New number of cafe guests:", cafeGuests);
-
-
-const maxGuests = 25;
-console.log("our place of guests:", maxGuests);
-
-// const maxGuests = 20;
-//console.log("max nummber of guests:", maxGuests);
-
-let myString = "coding is fun"
-console.log(myString.toLocaleUpperCase);
-console.log(myString.toLowerCase);
-
-
-
-
-
-
-
-
-
-let myNumber;
-
-myNumber = 20;
-
-myNumber = myNumber + 1;
-
-console.log(myNumber);
-
-myNumber +=1;
-
-console.log(myNumber);
-
-myNumber++;
-
-console.log(myNumber);
-console.log(myNumber +1);
-
-
-
-let varOne = 3;
-let varTwo = "3";
-
-console.log(varOne == varTwo)
-
-console.log(varOne === varTwo);
-
-console.log(varOne>varTwo);
-
-console.log(varOne>=7);
-
-console.log(varOne===varTwo || varOne <= 3 );
-
-
-let totalResult = 0;
-
-for (let i = 0; i <= 100; i++) {
-    totalResult += i;
+function callback(result) {
+    console.log("The result is:", result);
 }
 
-console.log("The total result is:", totalResult);
+multiply(7, 7, callback); 
+
+
+console.log("------------");
+console.log("------------");
+console.log("------------");
+console.log("------------");
 
 
 
 
-
-let number = 2;
-
-while (number <= 20) {
-    console.log(number);
-    number += 2;
-
-}
-
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-
-
-
-function person(name, age, livesIn) {
-    if (livesIn.livesInCity) {
-        return true;
-    } else if (livesIn.livesInTown) {
-        return false;
-    }
-}
-
-
-function describePerson(name, age, livesIn) {
-    const place = person(name, age, livesIn) ? "city" : "town";
-    return `${name} is ${age} years old and lives in the ${place}.`;
-}
-
-function person(name, age, livesIn) {
-    if (livesIn.livesInCity) {
-        return true;
-    } else if (livesIn.livesInTown) {
-        return false;
-    }
-}
-
-const personDetails = {
-    name: "Elvin",
-    age: 31,
-    livesIn: {
-        livesInCity: true,
-        livesInTown: false
-    }
+let callbackDva = (result) => {
+    console.log("The result is:", result);
 };
 
-console.log(describePerson(personDetails.name, personDetails.age, personDetails.livesIn));
+multiply(8, 8, callbackDva);
 
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
-console.log("------------------");
 
-function calculateCircleArea(radius) {
-    return Math.PI * radius ** 2;
+console.log("------------");
+console.log("------------");
+console.log("------------");
+console.log("------------");
+
+function greetUser(name, callback) {
+    const greetingMessage = `Hello, ${name}!`;
+    callback(greetingMessage);
 }
 
-// Example usage:
-const radius = 5;
-const area = calculateCircleArea(radius);
-console.log("The area of the circle with radius", radius, "is", area.toFixed(2));
 
+function callbackTwo(message) {
+    console.log(message);
+}
+
+greetUser("Elvin", callbackTwo); 
